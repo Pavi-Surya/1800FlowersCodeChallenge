a7efe40ce276701c03ef0dac416e094171520b0b
Pavi-Surya <rpavithra1995be@gmail.com>
2022-06-02 12:23:41 +0530

    Submission of assignment to Team - Review Changes 1

--- src/main/java/com/wipro/assignment/com/wipro/assignment/DemoApplication.java
+++ src/main/java/com/wipro/assignment/com/wipro/assignment/DemoApplication.java
@@ -1,14 +1,10 @@
 package com.wipro.assignment.com.wipro.assignment;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan({"com.wipro.codingtest"})
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}, scanBasePackages = {"com.wipro.codingtest"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
